4943085119c523d90a00aa89df16c69a357a84b6
jonayethossen <jonayethossen8921@gmail.com>
2025-01-23 15:56:20 +0600

    token genarate

--- controller/authController.js
+++ controller/authController.js
@@ -1,6 +1,7 @@
 const EmailValidateCheck = require("../helpers/ValidateEmail");
 const userModel = require("../model/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 async function registrationController(req, res) {
   let { name, email, password } = req.body;
@@ -35,13 +36,23 @@ async function loginController(req, res) {
   let { email, password } = req.body;
   let existinguser = await userModel.findOne({ email });
   if (existinguser) {
-    bcrypt.compare(password, existinguser.password, function (err, result) {
-      if (result) {
-        return res.status(200).send({ success: "Login Success" });
-      } else {
-        return res.status(404).send({ error: "Invalid Password" });
+    bcrypt.compare(
+      password,
+      existinguser.password,
+      async function (err, result) {
+        if (result) {
+          let info = await userModel.findOne({ email }).select("-password");
+          const token = jwt.sign({ info }, process.env.jwt_secret, {
+            expiresIn: "1d",
+          });
+          return res
+            .status(200)
+            .send({ success: "Login Success", data: existinguser, token });
+        } else {
+          return res.status(404).send({ error: "Invalid Password" });
+        }
       }
-    });
+    );
   } else {
     return res.send({ error: "Invalid Email" });
   }
